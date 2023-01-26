7442b00aecc4747ea418468e9cf1ee0db0e44b92
Gage <gagegunn11@gmail.com>
2023-01-26 01:24:09 -0700

    Update FS Test.py

--- RockSat-X/FS Test.py
+++ RockSat-X/FS Test.py
@@ -1,62 +1,11 @@
+"""
+File Structure Test
+This file is for testing the ability to access the 'File_Structure' file for use for the 22-23 RockSat-X Program along with giving examples on how it is used
+"""
 
-### Libraries ###
+### Librares ###
 import os
-from datetime import datetime
-
-class FileStructure:
-    
-    highest_id = 0
-    highest_id_path = {}
-
-    def __init__(self, data_path, folders, subfolders):
-        self.data_path = data_path
-        self.folders = folders
-        self.subfolders = subfolders
-        self.ID = 1
-        self.current_time = datetime.now().strftime("%m-%d-%Y_%Hhr-%Mmin-%Ssec")
-
-    def create_folders(self):
-        os.makedirs(self.data_path, exist_ok=True)
-        for folder in self.folders:
-            path = os.path.join(self.data_path, folder)
-            os.makedirs(path, exist_ok=True)
-            if folder in self.subfolders:
-                for subfolder in self.subfolders[folder]:
-                    subpath = os.path.join(path, subfolder)
-                    os.makedirs(subpath, exist_ok=True)
-        
-        find_highest_id(data_path)
-
-    def write_to_folder(self, folder_name, subfolder_name, data):
-        while True:
-            file_path = os.path.join(self.data_path, folder_name, subfolder_name, f"{self.ID} {fs.current_time}.txt")
-            folder_path = os.path.join(self.data_path, folder_name, subfolder_name)
-            if [file for file in os.listdir(folder_path) if file.split(stop_char)[0] == str(self.ID)]:
-                print(f"File with ID {self.ID} already exists.")
-                self.ID += 1
-            else:
-                with open(file_path, 'w') as file:
-                    file.write(data)
-                if fs.highest_id < self.ID:
-                    fs.highest_id = self.ID
-                    fs.highest_id_path[folder_path] = file_path
-                self.ID = 1
-                break
-        
-        find_highest_id(data_path)
-
-### Functions ###
-def find_highest_id(path):
-    for item in os.listdir(path):
-        item_path = os.path.join(path, item)
-        if os.path.isdir(item_path):
-            find_highest_id(item_path)
-        else:
-            # Check if the file name starts with an ID
-            id = item.split(" ")[0]
-            if id.isdigit():
-                if fs.ID > fs.highest_id:
-                    fs.highest_id_path[path] = item_path
+from File_Structure import FileStructure #<< This is our class for FileStructure
 
 
 ### Variables ###
@@ -73,7 +22,7 @@ fs = FileStructure(data_path, folders, subfolders)
 
 # Create the folders
 fs.create_folders()
-fs.write_to_folder('Flight_Log', '', 'Folders have been created\n')
+#fs.write_to_folder('Flight_Log', '', 'Folders have been created\n')
 
 # Creating files and writing the FIRST LINE of data
 test_number = str(20)
